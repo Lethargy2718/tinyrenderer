d34717da0addb72c0863bf61e84e777db79095a7
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2026-09-13 23:12:06 +0300

    feat: add bresenham line drawing algorithm

--- src/main.c
+++ src/main.c
@@ -1,21 +1,81 @@
-#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
 #include "tgaimage.h"
 
-int main(void) {
-    TGAImage img;
-    if (!tga_image_init(&img, 100, 100, TGA_RGB, tga_color(0, 0, 0, 255, 3))) {
-        fprintf(stderr, "failed to allocate framebuffer\n");
-        return 1;
+const char* filename = "framebuffer.tga";
+
+void line(int ax, int ay, int bx, int by, TGAImage *framebuffer, TGAColor color)
+{
+    int dx = abs(bx - ax);
+    int sx = (ax < bx) ? 1 : -1;
+
+    int dy = -abs(by - ay);
+    int sy = (ay < by) ? 1 : -1;
+
+    int err = dx + dy;
+
+    while (1)
+    {
+        tga_set(framebuffer, ax, ay, color);
+
+        if (ax == bx && ay == by)
+            break;
+
+        int e2 = 2 * err;
+
+        if (e2 >= dy)
+        {
+            err += dy;
+            ax += sx;
+        }
+
+        if (e2 <= dx)
+        {
+            err += dx;
+            ay += sy;
+        }
     }
+}
 
-    tga_set(&img, 50, 50, tga_color(255, 255, 255, 255, 3));
+int main(void)
+{
+    const int width = 64;
+    const int height = 64;
 
-    if (!tga_write_file(&img, "output.tga", true, true)) {
-        tga_image_free(&img);
+    const TGAColor white  = tga_color(255, 255, 255, 255, TGA_RGB);
+    const TGAColor green  = tga_color(  0, 255,   0, 255, TGA_RGB);
+    const TGAColor red    = tga_color(  0,   0, 255, 255, TGA_RGB);
+    const TGAColor blue   = tga_color(255, 128,  64, 255, TGA_RGB);
+    const TGAColor yellow = tga_color(  0, 200, 255, 255, TGA_RGB);
+    const TGAColor black  = tga_color(  0,   0,   0, 255, TGA_RGB);
+
+    TGAImage framebuffer;
+
+    if (!tga_image_init(&framebuffer, width, height, TGA_RGB, black))
         return 1;
-    }
 
-    tga_image_free(&img);
-    printf("wrote output.tga\n");
+    int ax = 7,  ay = 3;
+    int bx = 12, by = 37;
+    int cx = 62, cy = 53;
+
+    line(ax, ay, bx, by, &framebuffer, blue);
+    line(cx, cy, bx, by, &framebuffer, green);
+    line(cx, cy, ax, ay, &framebuffer, yellow);
+    line(ax, ay, cx, cy, &framebuffer, red);
+
+    tga_set(&framebuffer, ax, ay, white);
+    tga_set(&framebuffer, bx, by, white);
+    tga_set(&framebuffer, cx, cy, white);
+
+    tga_write_file(
+        &framebuffer,
+        filename,
+        true,
+        false
+    );
+
+    tga_image_free(&framebuffer);
+
     return 0;
 }
